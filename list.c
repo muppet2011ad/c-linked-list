c6201e217313ac1b936dfc822262eb8667b7c7ef
James Townsend <j.a.townsend2000@gmail.com>
2019-11-12 15:43:21 +0000

    Finished off list functions (as yet untested)

--- list.c
+++ list.c
@@ -7,9 +7,9 @@ int main(){
 }
 
 struct Node *newList(){
-    static struct Node head;
-    head.next = NULL;
-    return &head;
+    struct Node *head = malloc(sizeof(struct Node));
+    head->next = NULL;
+    return head;
 }
 
 struct Node *getNode(struct Node *head, int n){
@@ -36,4 +36,31 @@ void addToList(struct Node *new, struct Node *head, int n){
     struct Node *atarget = btarget->next;
     btarget->next = new;
     new->next = atarget;
+}
+
+void removeFromList(struct Node *head, int n){
+    struct Node *btarget = getNode(head, n-1);
+    struct Node *target = btarget->next;
+    struct Node *atarget = target->next;
+    btarget->next = atarget;
+    free(target);
+}
+
+int getListLen(struct Node *head){
+    int length = 1;
+    struct Node *sel = head;
+    while (sel->next != NULL){
+        length++;
+        sel = sel->next;
+    }
+    return length;
+}
+
+void deleteList(struct Node *head){
+    struct Node *sel = head;
+    while (sel->next != NULL){
+        struct Node *next = sel->next;
+        free(sel);
+        sel = next;
+    }
 }
