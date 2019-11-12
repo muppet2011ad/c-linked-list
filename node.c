cdcacd9e9410e391b90a38aa3aa117d086224942
James Townsend <j.a.townsend2000@gmail.com>
2019-11-12 15:51:15 +0000

    Added getter methods

--- node.c
+++ node.c
@@ -11,3 +11,26 @@ typedef struct Node{
     union Data data;
     struct Node *next;
 };
+
+struct Node *newNode(){
+    struct Node *new = malloc(sizeof(struct Node));
+    new->next = NULL;
+    return new;
+}
+
+int getNodeI(struct Node *node){
+    return node->data.i;
+}
+
+float getNodeF(struct Node *node){
+    return node->data.f;
+}
+
+char getNodeC(struct Node *node){
+    return node->data.c;
+}
+
+char *getNodeS(struct Node *node){
+    return &node->data.s;
+}
+
