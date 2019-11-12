fd45c9a48da8027f8a51b205c9183407e0030ae5
James Townsend <j.a.townsend2000@gmail.com>
2019-11-12 17:08:53 +0000

    Added a basic test - surprisingly no leaks

--- list.c
+++ list.c
@@ -2,8 +2,29 @@
 #include <stdlib.h>
 #include "node.c"
 
-int main(){
+struct Node *newList();
+struct Node *newListFromNode(struct Node *head);
+struct Node *getNode(struct Node *head, int n);
+void addToListEnd(struct Node *new, struct Node *head);
+struct Node *addToList(struct Node *new, struct Node *head, int n);
+struct Node *removeFromList(struct Node *head, int n);
+int getListLen(struct Node *head);
+void deleteList(struct Node *head);
 
+int main(){
+    printf("Hello World\n");
+    struct Node *list1 = newList();
+    setNodeI(list1,1337);
+    addToListEnd(newNodeI(255), list1);
+    printf("%d\n",getListLen(list1));
+    printf("%d\n",getNodeI(getNode(list1,1)));
+    addToList(newNodeI(2019),list1,1);
+    printf("%d\n",getNodeI(getNode(list1,1)));
+    removeFromList(list1,1);
+    printf("%d\n",getNodeI(getNode(list1,1)));
+    deleteList(list1);
+    printf("%d\n",sizeof(struct Node));
+    return 0;
 }
 
 struct Node *newList(){
@@ -36,19 +57,36 @@ void addToListEnd(struct Node *new, struct Node *head){
     sel->next = new;
 }
 
-void addToList(struct Node *new, struct Node *head, int n){
-    struct Node *btarget = getNode(head, n-1);
-    struct Node *atarget = btarget->next;
-    btarget->next = new;
-    new->next = atarget;
+struct Node *addToList(struct Node *new, struct Node *head, int n){
+    if (n != 0){
+        struct Node *btarget = getNode(head, n-1);
+        struct Node *atarget = btarget->next;
+        btarget->next = new;
+        new->next = atarget;
+        return head;
+    }
+    else{
+        new->next = head;
+        return new;
+    }
+
 }
 
-void removeFromList(struct Node *head, int n){
-    struct Node *btarget = getNode(head, n-1);
-    struct Node *target = btarget->next;
-    struct Node *atarget = target->next;
-    btarget->next = atarget;
-    free(target);
+struct Node *removeFromList(struct Node *head, int n){
+    if (n != 0){
+        struct Node *btarget = getNode(head, n-1);
+        struct Node *target = btarget->next;
+        struct Node *atarget = target->next;
+        btarget->next = atarget;
+        free(target);
+        return head;
+    }
+    else{
+        struct Node *next = head->next;
+        free(head);
+        return next;
+    }
+
 }
 
 int getListLen(struct Node *head){
@@ -68,4 +106,5 @@ void deleteList(struct Node *head){
         free(sel);
         sel = next;
     }
+    free(sel);
 }
